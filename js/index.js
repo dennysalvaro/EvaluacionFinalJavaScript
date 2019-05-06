1734b7ce71ce05b91762cdbf43d3ee6ff2ba27bf
DESKTOP-M87ESQG\DENNYS <dennysalvaro01@gmail.com>
2019-05-05 21:40:24 -0500

    Signed-off-by: DESKTOP-M87ESQG\DENNYS <dennysalvaro01@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,40 +1,42 @@
-var calculadora = {
+/* DENNYS HUAMAN TRABAJO FINAL JAVASCRIPT*/
 
+var calculadora = {
+//1.- Asignamos variables auxiliares con las que trabajaremos
 	visor: document.getElementById("display"),
-	valorVisor: "0",
+	valorPantalla: "0",
 	operacion: "",
-	primerValor: 0,
-	segundoValor: 0,
-	ultimoValor: 0,
+	firstValue: 0,
+	secondValue: 0,
+	lastValor: 0,
 	resultado: 0,
-	auxTeclaIgual: false, // Para permitir ingreso consecutivo
+	ayudaTeclaIgual: false, // Variable que permitira el ingreso consecutivo de valores
 
 	init: (function(){
-		this.asignarEventosFormatoBotones(".tecla");
-		this.asignarEventosaFuncion();
+		this.colocarEventosParaBotones(".tecla");
+		this.colocarEventosParaFuncion();
 	}),
 
-	//Eventos de formato de botones
+	//2.- Eventos para formato de botones
 
-	asignarEventosFormatoBotones: function(selector){
+	colocarEventosParaBotones: function(selector){
 		var x = document.querySelectorAll(selector);
 		for (var i = 0; i<x.length;i++) {
-			x[i].onmouseover = this.eventoAchicaBoton;
-			x[i].onmouseleave = this.eventoVuelveBoton;
+			x[i].onmouseover = this.eventoReduceBoton;
+			x[i].onmouseleave = this.eventoBotonFormaInicial;
 		};
 	},
 
-	eventoAchicaBoton: function(event){
-		calculadora.AchicaBoton(event.target);
+	eventoReduceBoton: function(event){
+		calculadora.ReduceBoton(event.target);
 	},
 
-	eventoVuelveBoton: function(event){
-		calculadora.AumentaBoton(event.target);
+	eventoBotonFormaInicial: function(event){
+		calculadora.FormaInicialBoton(event.target);
 	},
 
-	//Formato de botones
+	//2.1.- estableciendo como cambiaran el formato de botones
 
-	AchicaBoton: function(elemento){
+	ReduceBoton: function(elemento){
 		var x = elemento.id;
 		if (x=="1" || x=="2" || x=="3" || x=="0" || x=="igual" || x=="punto" ) {
 			elemento.style.width = "28%";
@@ -48,7 +50,7 @@ var calculadora = {
 		}
 	},
 
-	AumentaBoton: function(elemento){
+	FormaInicialBoton: function(elemento){
 		var x = elemento.id;
 		if (x=="1" || x=="2" || x=="3" || x=="0" || x=="igual" || x=="punto" ) {
 			elemento.style.width = "29%";
@@ -61,135 +63,153 @@ var calculadora = {
 		elemento.style.height = "62.91px";
 		}
 	},
-  //---Fin evento de formato de botones
 
-	//Eventos de función de calculadora
+	//2.2---Fin evento para formato de botones
 
-  asignarEventosaFuncion: function(){
-    document.getElementById("0").addEventListener("click", function() {calculadora.ingresoNumero("0");});
-    document.getElementById("1").addEventListener("click", function() {calculadora.ingresoNumero("1");});
-    document.getElementById("2").addEventListener("click", function() {calculadora.ingresoNumero("2");});
-    document.getElementById("3").addEventListener("click", function() {calculadora.ingresoNumero("3");});
-    document.getElementById("4").addEventListener("click", function() {calculadora.ingresoNumero("4");});
-    document.getElementById("5").addEventListener("click", function() {calculadora.ingresoNumero("5");});
-    document.getElementById("6").addEventListener("click", function() {calculadora.ingresoNumero("6");});
-    document.getElementById("7").addEventListener("click", function() {calculadora.ingresoNumero("7");});
-    document.getElementById("8").addEventListener("click", function() {calculadora.ingresoNumero("8");});
-    document.getElementById("9").addEventListener("click", function() {calculadora.ingresoNumero("9");});
-    document.getElementById("on").addEventListener("click", function() {calculadora.borrarVisor();});
-    document.getElementById("sign").addEventListener("click", function() {calculadora.cambiarSigno();});
-    document.getElementById("punto").addEventListener("click", function() {calculadora.ingresoDecimal();});
-    document.getElementById("igual").addEventListener("click", function() {calculadora.verResultado();});
-    document.getElementById("raiz").addEventListener("click", function() {calculadora.ingresoOperacion("raiz");});
-    document.getElementById("dividido").addEventListener("click", function() {calculadora.verResultado("/");});
-    document.getElementById("por").addEventListener("click", function() {calculadora.verResultado("*");});
-    document.getElementById("menos").addEventListener("click", function() {calculadora.verResultado("-");});
-    document.getElementById("mas").addEventListener("click", function() {calculadora.verResultado("+");});
-  },
+	//3.- Eventos para hacer funcionar la calculadora
 
-  //---Fin eventos de función calculadora
+	colocarEventosParaFuncion: function(){
+		document.getElementById("0").addEventListener("click", function() {calculadora.ingresarNumero("0");});
+		document.getElementById("1").addEventListener("click", function() {calculadora.ingresarNumero("1");});
+		document.getElementById("2").addEventListener("click", function() {calculadora.ingresarNumero("2");});
+		document.getElementById("3").addEventListener("click", function() {calculadora.ingresarNumero("3");});
+		document.getElementById("4").addEventListener("click", function() {calculadora.ingresarNumero("4");});
+		document.getElementById("5").addEventListener("click", function() {calculadora.ingresarNumero("5");});
+		document.getElementById("6").addEventListener("click", function() {calculadora.ingresarNumero("6");});
+		document.getElementById("7").addEventListener("click", function() {calculadora.ingresarNumero("7");});
+		document.getElementById("8").addEventListener("click", function() {calculadora.ingresarNumero("8");});
+		document.getElementById("9").addEventListener("click", function() {calculadora.ingresarNumero("9");});
+		document.getElementById("on").addEventListener("click", function() {calculadora.borrarPantalla();});
+		document.getElementById("sign").addEventListener("click", function() {calculadora.cambiarSigno();});
+		document.getElementById("punto").addEventListener("click", function() {calculadora.ingresarDecimal();});
+		document.getElementById("igual").addEventListener("click", function() {calculadora.mostrarResultado();});
+		document.getElementById("raiz").addEventListener("click", function() {calculadora.ingresarOperacion("raiz");});
+		document.getElementById("dividido").addEventListener("click", function() {calculadora.ingresarOperacion("/");});
+		document.getElementById("por").addEventListener("click", function() {calculadora.ingresarOperacion("*");});
+		document.getElementById("menos").addEventListener("click", function() {calculadora.ingresarOperacion("-");});
+		document.getElementById("mas").addEventListener("click", function() {calculadora.ingresarOperacion("+");});
+	},
 
-  //Funcion de teclas de calculadora
+	//3.1---Fin eventos de función calculadora
 
-  borrarVisor: function(){
-    this.valorVisor = "0";
-    this.operacion = "";
-    this.primerValor = 0;
-    this.segundoValor = 0;
-    this.resultado = 0;
-    this.Operacion = "";
-    this.auxTeclaIgual = false;
-    this.ultimoValor = 0;
-    this.updateVisor();
-  },
+	//4.- Funciones para asignar teclas de calculadora
 
-  cambiarSigno: function(){
-    if (this.valorVisor !="0") {
-      var aux;
-      if (this.valorVisor.charAt(0)=="-") {
-        aux = this.valorVisor.slice(1);
-      } else {
-				aux = "-" + this.valorVisor;
+	borrarPantalla: function(){
+
+	    this.valorPantalla = "0";
+		this.operacion = "";
+		this.firstValue = 0;
+		this.secondValue = 0;
+		this.resultado = 0;
+		this.Operación = "";
+		this.ayudaTeclaIgual = false;
+		this.lastValor = 0;
+		this.actualizarPantalla();
+	},
+
+	cambiarSigno: function(){
+		if (this.valorPantalla !="0") {
+			var aux;
+			if (this.valorPantalla.charAt(0)=="-") {
+				aux = this.valorPantalla.slice(1);
+			}	else {
+				aux = "-" + this.valorPantalla;
 			}
-			this.valorVisor = "";
-			this.valorVisor = aux;
-			this.updateVisor();
-    }
-  },
-
-	ingresoDecimal: function(){
-		if (this.valorvisor.indexOf(".")== -1) {
-			if (this.valorVisor == "") {
-				this.valorVisor = this.valorVisor + "0.";
-			} esle {
-				this.valorVisor = this.valorVisor + ".";
-			}
-			this.updateVisor();
+		this.valorPantalla = "";
+		this.valorPantalla = aux;
+		this.actualizarPantalla();
 		}
 	},
 
-	ingresoNumero: function(valor){
-		if (this.valorVisor.length < 8) {
-			if (this.valorVisor=="0") {
-				this.valorVisor = "";
-				this.valorVisor = this.valorVisor + valor;
+	ingresarDecimal: function(){
+		if (this.valorPantalla.indexOf(".")== -1) {
+			if (this.valorPantalla == ""){
+				this.valorPantalla = this.valorPantalla + "0.";
 			} else {
-				this.valorVisor = this.valorVisor + valor;
+				this.valorPantalla = this.valorPantalla + ".";
 			}
-			this.updateVisor();
+			this.actualizarPantalla();
 		}
 	},
 
-	ingresoOperacion: function(oper){
-		this.primerValor = parseFloat(this.valorVisor);
-		this.valorVisor = "";
+	ingresarNumero: function(valor){
+		if (this.valorPantalla.length < 8) {
+
+			if (this.valorPantalla=="0") {
+				this.valorPantalla = "";
+				this.valorPantalla = this.valorPantalla + valor;
+			} else {
+				this.valorPantalla = this.valorPantalla + valor;
+			}
+		this.actualizarPantalla();
+		}
+	},
+
+	ingresarOperacion: function(oper){
+		this.firstValue = parseFloat(this.valorPantalla);
+		this.valorPantalla = "";
 		this.operacion = oper;
-		this.auxTeclaIgual = false;
-		this.updateVisor();
+		this.ayudaTeclaIgual = false;
+		this.actualizarPantalla();
 	},
 
-	verResultado: function(){
-		if(!this.auxTeclaIgual){
-			this.segundoValor = parseFloat(this.valorVisor);
-			this.ultimoValor = this.segundoValor;
+	mostrarResultado: function(){ // TECLA IGUAL
 
-			//Calculo el Resultado
-			this.realizarOperacion (this.primerValor, this.segundoValor, this.operacion);
-		} else {  //siguientes veces que presiono igual realizar operacion
-			//Calculo el Resultado
-			this.realizarOperacion(this.primerValor, this.ultimoValor, this.operacion);
+		if(!this.ayudaTeclaIgual){ //Primer vez que presiono igual
+			this.secondValue = parseFloat(this.valorPantalla);
+			this.lastValor = this.secondValue;
+
+		//Calculo el resultado
+			this.realizarOperacion(this.firstValue, this.secondValue, this.operacion);
+
+		} else { //Siguientes veces que presiono igualrealizarOperacion
+		//Calculo el resultado
+		this.realizarOperacion(this.firstValue, this.lastValor, this.operacion);
 		}
 
-		//Almacena el resultado como primer valor para poder seguir operando
-		this.primerValor = this.resultado;
+		//Almaceno el resultado como primer valor para poder seguir operando
+		this.firstValue = this.resultado;
 
-		//borra el visor y lo reemplazo por el resultado
-		this.valorVisor = "";
+		//Borro el visor y lo reemplazo por el resultado
+		this.valorPantalla = "";
 
-		//verifica el largo del resultado para recortarlo si hace falta
+		//verifico el largo del resultado para recortarlo si hace falta
 
-		if (this.resultado.toString().length < 9) {
-			this.valorVisor = this.resultado.toString();
+		if (this.resultado.toString().length < 9){
+			this.valorPantalla = this.resultado.toString();
 		} else {
-			this.valorVisor = this.resultado.toString().slice(0,8) + "...";
+			this.valorPantalla = this.resultado.toString().slice(0,8) + "...";
 		}
 
-		//Auxiliar para indicar si ya se presiono la tecla igual, para clacular sobre el ultimo valor
+		//Auxiliar para indicar si ya se presionó la tecla igual, para calcular sobre el último valor
 
-		this.auxTeclaIgual = true;
-		this.updateVisor();
+		this.ayudaTeclaIgual = true;
+		this.actualizarPantalla();
 
+	},
 
-
+	realizarOperacion: function(firstValue, secondValue, operacion){
+		switch(operacion){
+			case "+":
+				this.resultado = eval(firstValue + secondValue);
+			break;
+			case "-":
+				this.resultado = eval(firstValue - secondValue);
+			break;
+			case "*":
+				this.resultado = eval(firstValue * secondValue);
+			break;
+			case "/":
+				this.resultado = eval(firstValue / secondValue);
+			break;
+			case "raiz":
+				this.resultado = eval(Math.sqrt(firstValue));
 		}
+	},
+
+	actualizarPantalla: function(){
+		this.visor.innerHTML = this.valorPantalla;
 	}
-
-
-
-
-
-
-
 
 };
 
